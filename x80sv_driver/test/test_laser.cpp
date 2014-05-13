22de9c889e09e80909c8671b86b3ae6b33289caf
Windel Bouwman <windel@windel.nl>
2014-05-13 16:27:36 +0200

    Added test harnass to x80sv_driver

---
+++ x80sv_driver/test/test_laser.cpp
@@ -0,0 +1,17 @@
+
+#include <gtest/gtest.h>
+
+//using namespace geometry_msgs;
+
+TEST(LaserTestSuite, testSomeFunction)
+{
+    // pass
+}
+
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+

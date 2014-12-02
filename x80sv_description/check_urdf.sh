0168d1344ced94d01b7af1967b2885e301aef4fb
Windel Bouwman <windel.bouwman@gmail.com>
2014-12-02 08:50:01 +0100

    Fix urdf checker

--- x80sv_description/check_urdf.sh
+++ x80sv_description/check_urdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rosrun xacro xacro.py urdf/x80sv.xacro
-rosrun xacro xacro.py urdf/x80sv.xacro | check_urdf -
+rosrun xacro xacro.py urdf/x80sv.xacro -o /tmp/x80sv.urdf
+check_urdf /tmp/x80sv.urdf
 
 
